b8209791115ea1803281db595e4d1ee45702caa3
CelesteOj22 <93403946+CelesteOj22@users.noreply.github.com>
2022-04-07 01:13:24 -0300

    Delete Solvd Directory
    I created a new branch to organize the rest of the homework but I had to create a new project

--- Solvd/src/main/java/org/Solvd/supermarket/exceptions/LessThanThreeProductsException.java
+++
@@ -1,9 +0,0 @@
-package org.Solvd.supermarket.exceptions;
-
-import java.io.IOException;
-
-public class LessThanThreeProductsException extends IOException {
-    public LessThanThreeProductsException(){}
-    public LessThanThreeProductsException(String message){super(message);}
-    public LessThanThreeProductsException(String message,Throwable cause){super(message,cause);}
-}
--- Solvd/src/main/java/org/Solvd/supermarket/exceptions/NotEnoughFundsException.java
+++
@@ -1,7 +0,0 @@
-package org.Solvd.supermarket.exceptions;
-
-public class NotEnoughFundsException extends Exception{
-    public NotEnoughFundsException(){}
-    public NotEnoughFundsException(String message){super(message);}
-    public NotEnoughFundsException(String message,Throwable cause){super(message,cause);}
-}
