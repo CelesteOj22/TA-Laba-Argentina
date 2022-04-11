307427f36676e164cb2b9be7b2a4cdabb5df2a9e
CelesteOj22 <93403946+CelesteOj22@users.noreply.github.com>
2022-04-10 23:40:33 -0300

    Delete Solvd old directory

--- Solvd/src/main/java/org/Solvd/supermarket/exceptions/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.Solvd.supermarket.exceptions;
-
-public class EmployeeNotFoundException extends RuntimeException{
-    public EmployeeNotFoundException(){}
-    public EmployeeNotFoundException(String message){super(message);}
-    public EmployeeNotFoundException(String message,Throwable cause){super(message,cause);}
-}
